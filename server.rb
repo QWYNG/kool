9c77be45819dbba97ba5ed53062d0dfb234ff8d3
qwyng <ikusawasi@gmail.com>
2024-06-29 11:45:01 +0900

    add visualization server

--- server.rb
+++ server.rb
@@ -1,39 +1,47 @@
 require 'bundler/inline'
 require 'sinatra'
-require 'nokogiri'
+require 'rexml/document'
+require 'ruby-graphviz'
+
 
 get '/', :provides => 'html' do
   send_file 'view/index.html'
 end
 
-post '/run-krun' do
-  content_type :json
-
+post '/run-krun', provides: 'svg'  do
   request_data = JSON.parse(request.body.read)
   filename = request_data['filename']
   depth = request_data['depth']
-  command = "krun #{filename} --depth #{depth} > view/out.xml"
+  command = "krun #{filename} --depth #{depth} > build/out.xml"
   puts 'Running command: ' + command
 
   status = system(command)
 
   if status
-    xml_data = File.read('view/out.xml')
-    transformed_xml = transform_xml(xml_data)
-    transformed_xml.to_json
+    xml_data = File.read('build/out.xml')
+    doc = REXML::Document.new(xml_data)
+    graph = GraphViz.new(:G, type: :digraph)
+    add_nodes(graph, doc)
+    graph.output(svg: 'build/xml_tree.svg')
+
+    File.read('build/xml_tree.svg')
   else
     status 500
     { error: 'Error executing command' }.to_json
   end
 end
 
-def transform_xml(xml)
-  xslt_path = 'view/out.xslt'
-  xslt = Nokogiri::XSLT(File.read(xslt_path))
-  doc = Nokogiri::XML(xml)
-  transformed = xslt.transform(doc)
-  transformed.to_s
+def add_nodes(graph, xml_node, parent_node = nil)
+  if xml_node.is_a?(REXML::Text)
+    return if xml_node.value.strip.empty?
+    graph.add_nodes(xml_node.value)
+    graph.add_edges(parent_node, xml_node.value) if parent_node
+    return
+  end
+  node = graph.add_nodes(xml_node.name)
+  graph.add_edges(parent_node, node) if parent_node
+
+  xml_node.children.each do |child|
+    add_nodes(graph, child, node)
+  end
 end
-
-
-
